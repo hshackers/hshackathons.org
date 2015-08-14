003795cd0ce32fbea869a17faa29e315727164b7
Victor Lourng <hello@victorlourng.com>
2015-08-14 12:48:52 -0400

    Add Gulp

---
+++ Gulpfile.js
@@ -0,0 +1,54 @@
+var gulp = require('gulp'),
+    less = require('gulp-less'),
+    path = require('path'),
+    prefix = require('gulp-autoprefixer'),
+    minifyCSS = require('gulp-minify-css')
+    server = require('tiny-lr')(),
+    livereload = require('gulp-livereload'),
+    connect = require('gulp-connect');
+
+gulp.task('styles', function() {
+    gulp
+        .src('./css/less/main.less')
+        .pipe(less({
+            paths: [path.join(__dirname, 'app', 'assets', 'less')]
+        }))
+        .pipe(prefix("last 1 version", "> 1%", "ie 8", "ie 7"))
+        .pipe(minifyCSS())
+        .pipe(gulp.dest('./app/css'))
+        .pipe(livereload(server));
+});
+
+gulp.task('connect', connect.server({
+    root: __dirname,
+    port: 2015,
+    livereload: false
+}));
+
+gulp.task('scripts', function() {
+    gulp
+        .src('./js/**/*.js')
+        .pipe(livereload(server));
+});
+
+gulp.task('views', function() {
+    gulp
+        .src('./*.html')
+        .pipe(livereload(server));
+});
+
+// The default task (called when you run `gulp`)
+gulp.task('default',['connect'], function() {
+    server.listen(35729, function(err) {
+        if (err) return console.log(err);
+        gulp.watch('/css/less/**/*.less', function() {
+            gulp.run('styles');
+        });
+        gulp.watch('/js/**/*.js', function() {
+            gulp.run('scripts');
+        });
+        gulp.watch('/*.html', function() {
+            gulp.run('views');
+        });
+    });
+});
